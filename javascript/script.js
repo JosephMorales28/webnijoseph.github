b860ea2be879ba5cc3e7028c9716a1c08da6ac8f
JosephMorales28 <117571756+JosephMorales28@users.noreply.github.com>
2022-12-02 11:33:10 +0800

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,4 @@
+const webs=[{title:"WebniJoseph",coder:"Joseph Morales"}];
+console.table(webs);
+console.log("this website is created by Joseph Anthony V. Morales");
+console.log("On September 29,2022");
