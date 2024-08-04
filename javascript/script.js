cf6368114fc7683f4679c1ed76696f379895290c
Joseph Morales <117571756+JosephMorales28@users.noreply.github.com>
2024-08-04 10:04:39 +0800

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,13 @@
+let statement;
+statement="I'm Joseph Morales, a graphic designer and web developer in manila.I'm very passionate,creative in my design works and arts. I'm very interested to join on your team projects and managemment design and art industry,to upgrade my skill level and more knowledge in professional design ideas and creativity.";
+
+document.getElementById("demo").innerHTML=statement;
+
+let coder;
+
+coder="Joseph Anthony V. Morales"
+
+console.log("code by:" + coder);
 const webs=[{title:"WebniJoseph",coder:"Joseph Morales"}];
 console.table(webs);
 console.log("this website is created by Joseph Anthony V. Morales");
